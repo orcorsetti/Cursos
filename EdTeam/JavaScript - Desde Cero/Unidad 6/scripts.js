8820f805870ea26d325cd3d4e88c61a7e041149c
Ornela Cosetti <orcorsetti@gmail.com>
2020-05-06 17:43:20 -0300

    Unidad6-Avance

--- EdTeam/JavaScript - Desde Cero/Unidad 6/scripts.js
+++ EdTeam/JavaScript - Desde Cero/Unidad 6/scripts.js
@@ -11,3 +11,19 @@ let gato = {
 console.log(gato.nombre)
 console.log(gato.correr)
 console.log(gato.correr())
+
+let user = {
+    nombre: 'Ornela'
+}
+
+console.log(user.nombre)
+console.log(user['nombre'])
+
+let a = 'Hola', b = 'mundo'
+
+let saludo = {
+    [a + b]: 'Variable holamundo'
+}
+
+console.log(saludo['holamundo'])
+ 
