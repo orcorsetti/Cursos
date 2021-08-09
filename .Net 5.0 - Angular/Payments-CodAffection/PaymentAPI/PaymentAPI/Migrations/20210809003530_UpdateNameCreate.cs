7c21cf28998ff9dffdf9e57ff4c2c7e93d0c0991
Ornela Cosetti <48458591+orcorsetti@users.noreply.github.com>
2021-08-08 22:08:58 -0300

    Initial-Commit

---
+++ .Net 5.0 - Angular/Payments-CodAffection/PaymentAPI/PaymentAPI/Migrations/20210809003530_UpdateNameCreate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentAPI.Migrations
+{
+    public partial class UpdateNameCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EpirationDate",
+                table: "PaymentDetails",
+                newName: "ExpirationDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpirationDate",
+                table: "PaymentDetails",
+                newName: "EpirationDate");
+        }
+    }
+}
