36da1ed744536657efc06b7fe089b09e16ff30e1
Ornela Cosetti <48458591+orcorsetti@users.noreply.github.com>
2021-08-13 00:05:35 -0300

    Agrego Validaciones en el Model

---
+++ .Net 5.0 - Angular/Payments-CodAffection/PaymentAPI/PaymentAPI/Migrations/20210813024614_FieldsUpdate.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentAPI.Migrations
+{
+    public partial class FieldsUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SecurityCode",
+                table: "PaymentDetails",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpirationDate",
+                table: "PaymentDetails",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardOwnerName",
+                table: "PaymentDetails",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "PaymentDetails",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SecurityCode",
+                table: "PaymentDetails",
+                type: "nvarchar(3)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3)",
+                oldMaxLength: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpirationDate",
+                table: "PaymentDetails",
+                type: "nvarchar(5)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardOwnerName",
+                table: "PaymentDetails",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "PaymentDetails",
+                type: "nvarchar(16)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+        }
+    }
+}
--- .Net 5.0 - Angular/Payments-CodAffection/PaymentAPI/PaymentAPI/Models/PaymentDetail.cs
+++ .Net 5.0 - Angular/Payments-CodAffection/PaymentAPI/PaymentAPI/Models/PaymentDetail.cs
@@ -13,15 +13,27 @@ namespace PaymentAPI.Models
         public int PaymentDetailId { get; set; }
 
         [Column(TypeName ="nvarchar(100)")]
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z ]*$")]
         public string CardOwnerName { get; set; }
 
         [Column(TypeName = "nvarchar(16)")]
+        [Required]
+        [StringLength(16)]
+        [RegularExpression(@"^(\d{16})$")]
         public string CardNumber { get; set; }
 
         [Column(TypeName = "nvarchar(5)")]
+        [Required]
+        [StringLength(5)]
+        [RegularExpression(@"^(\d\d\/\d\d)$")]
         public string ExpirationDate { get; set; }
 
         [Column(TypeName = "nvarchar(3)")]
+        [Required]
+        [StringLength(3)]
+        [RegularExpression(@"^(\d{3})$")]
         public string SecurityCode { get; set; }
     }
 }
